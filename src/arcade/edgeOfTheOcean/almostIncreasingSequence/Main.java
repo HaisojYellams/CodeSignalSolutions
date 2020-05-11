82fe17f32f39f3393ac23849b379e8425154aa3a
Josiah Smalley <haisoj.yellams@gmail.com>
2020-05-10 20:23:05 -0500

    Solves problems 6-8 of Arcade: Intro.

---
+++ src/arcade/edgeOfTheOcean/almostIncreasingSequence/Main.java
@@ -0,0 +1,23 @@
+package arcade.edgeOfTheOcean.almostIncreasingSequence;
+
+public class Main {
+    boolean almostIncreasingSequence(int[] sequence) {
+        int dipIndex = -1;
+
+        for (int i = 0; i < sequence.length - 1; i++) {
+            if (sequence[i] >= sequence[i + 1]) {
+                if (dipIndex >= 0) {
+                    return false;
+                }
+
+                dipIndex = i;
+            }
+        }
+
+        if (dipIndex == -1 || dipIndex == 0 || dipIndex == sequence.length - 2) {
+            return true;
+        }
+
+        return sequence[dipIndex - 1] < sequence[dipIndex + 1] || sequence[dipIndex] < sequence[dipIndex + 2];
+    }
+}
